6925aad581b04da0497410f88885982817723586
Fawad Bashir <31212077+fawadbashir@users.noreply.github.com>
2019-09-30 01:47:02 +0500

    fixing a mistake

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,16 +1,16 @@
 const request = require('request')
 
 
-const foreCast = (longitude,latitude, callback) =>{
+const foreCast = (longitude, latitude, callback) => {
     const url = `https://api.darksky.net/forecast/48f59c5fab71be9338afb59c4811b4f5/${latitude},${longitude}?units=si`
 
-    request({url, json : true},(error, response)=>{
+    request({ url, json: true }, (error, response) => {
         if (error) {
-            callback('cannot reach the newtork',undefined)
+            callback('cannot reach the newtork', undefined)
         } else if (!response.body.currently) {
-            callback('unable to find forecast. Try another search',undefined)
+            callback('unable to find forecast. Try another search', undefined)
         } else {
-            callback(undefined,` ${response.body.hourly.summary} Currently ${response.body.currently.temperature} degrees out. There is  ${response.body.currently.precipProbability}%  chance of Rain`)
+            callback(undefined, ` ${response.body.hourly.summary} Currently ${response.body.currently.temperature} degrees out. There is  ${response.body.currently.precipProbability}%  chance of Rain. ${response.body.daily.summary}`)
         }
     })
 }
